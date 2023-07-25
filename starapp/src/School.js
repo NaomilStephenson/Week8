f5c162a2a3155e1e8ab7a19e82e6f5db89e3e9e2
=Naomi <=Naomi@gmail.com>
2023-07-25 16:53:29 +0100

    adjusted School.js

--- starapp/src/School.js
+++ starapp/src/School.js
@@ -5,20 +5,21 @@ function School(){
         {Subject: "English", Teacher: "Ms. Smith", Room: "E4"},
         {Subject: "Science", Teacher: "Mr. Walker", Room: "S11"}
     ];
-    function insertSubject(){
+    function Add(){
         subjects.push({Subject: "ICT", Teacher: "Mrs. Townsend", Room: "S15"});
+        AddSubject(subjects);
         alert(subjects.length);
         console.log(subjects);
     };
-    let [subject,addSubject]=useState(subjects);
+    let [lesson,AddSubject]=useState(subjects);
     return(
         <>
-            <input type="button" value="New Lesson" onclick={insertSubject}/>
+            <input type="button" value="New Lesson" onclick={Add}/>
             <table>
                 <tr>
                     <td><b>Subject</b></td><td><b>Teacher</b></td><td><b>Room</b></td>
                 </tr>
-                {subjects.map((lesson)=>
+                {lesson.map((lesson)=>
                     <tr>
                         <td>{lesson.Subject}</td>
                         <td>{lesson.Teacher}</td>
